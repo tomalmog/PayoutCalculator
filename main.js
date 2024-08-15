babe565d40a1917a4ddd7d1bf1e9644ae93cc4b2
tomalmog <57159577+tomalmog@users.noreply.github.com>
2024-08-14 20:25:44 -0400

    Update main.js

--- main.js
+++ main.js
@@ -11,11 +11,11 @@ const calculateButton = document.getElementById("calculate");
 calculateButton.addEventListener('click', calculate);
 
 function calculate () {
-    var total = buyInInput.value * -5;
-    total += whiteInput.value * 1;
-    total += redInput.value * 0.5;
-    total += blueInput.value * 0.1;
-    total += greenInput.value * 0.02;
+    var total = buyInInput.value * -10;
+    total += whiteInput.value * 2;
+    total += redInput.value * 1;
+    total += blueInput.value * 0.2;
+    total += greenInput.value * 0.04;
 
     total = total.toFixed(2);
 
